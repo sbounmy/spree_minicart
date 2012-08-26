104b561501f4203735235b06b773ca5d7f1a288f
Stephane Bounmy <stephanebounmy@gmail.com>
2012-08-26 02:36:42 +0200

    added backorder test and refactored

--- app/controllers/orders_controller_decorator.rb
+++ app/controllers/orders_controller_decorator.rb
@@ -1,13 +1,9 @@
 module Spree
   OrdersController.class_eval do
-    before_filter :set_current_order, :only => [:populate]
 
     respond_to :js, :only => [:populate, :update]
 
     ssl_allowed :populate, :update
 
-    def set_current_order
-      current_order(true)
-    end
   end
 end
--- spec/requests/minicart_spec.rb
+++ spec/requests/minicart_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 feature "minicart", :js => true do
-  background { @product = FactoryGirl.create(:product, :name => "ror mug", :price => 30) }
+  background { @product = FactoryGirl.create(:product, :name => "ror mug", :price => 30, :on_hand => 2) }
 
   scenario "customer should be able to add and delete a product in the minicart" do
     visit spree.products_path
@@ -100,4 +100,38 @@ feature "minicart", :js => true do
       visit spree.products_path
     }.to_not change(Spree::Order, :count)
   end
+
+  scenario 'with allowbackorders to false minicart should not be able to item multiple time' do
+    reset_spree_preferences do |config|
+      config[:allow_backorders] = false
+    end
+    @product.master.update_column :count_on_hand, 1
+
+    visit spree.products_path
+    click_link("ror mug")
+
+    within("li#link-to-cart") do
+      page.should have_content("Cart: (Empty)")
+    end
+
+    click_button "Add To Cart"
+    click_button "Add To Cart"
+
+    within("#link-to-cart a") do
+      page.should have_content("(2)")
+    end
+
+    within("#minicart") do
+      page.should have_content("ror mug")
+      page.should have_content('Insufficient stock available, only 1 remaining')
+    end
+
+    visit spree.products_path
+
+    within("#link-to-cart a") do
+      page.should have_content("(1)")
+    end
+
+  end
+
 end
